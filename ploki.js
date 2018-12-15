acbba1bdb683fec7864203ce1b7aaf5cf0107eba
Romuald Brunet <romuald@chivil.com>
2018-12-15 11:11:39 +0100

    Remove angular dependency, switch to vanillajs

--- ploki.js
+++ ploki.js
@@ -1,85 +1,134 @@
-//+ Jonas Raoni Soares Silva
-//@ http://jsfromhell.com/array/shuffle [v1.0]
-function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
+(function() {
+	//+ Jonas Raoni Soares Silva
+	//@ http://jsfromhell.com/array/shuffle [v1.0]
+	function shuffle(o){ //v1.0
+	    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	    return o;
+	};
 
-var app = angular.module('plokiploki', []);
-app.directive('plokiWheel', ['$parse', function($parse){
-    return {
-        require: 'ngModel',
-        link: function(scope, element, attr, ngModel) {
-            element.bind('wheel', function(event, delta){
-                var foo = event.deltaY > 0 ? 1 : -1;
+	function int(i) {
+		return parseInt(i, 10);
+	}
 
-                var allowed = scope.$eval(attr.plokiWheel);
-                var value = ngModel.$modelValue + foo;
-                event.preventDefault();
+	function initSelect(select, arr) {
+		arr.forEach(function(i) {
+			var opt = document.createElement('option');
+			opt.value = i;
+			opt.innerText = i;
+			select.appendChild(opt);
+		});
+	}
 
-                if (allowed.indexOf(value) != -1)  {
-                    ngModel.$setViewValue(ngModel.$modelValue + foo, event);
-                    scope.$digest()
-                }
-            });
-        }
-    };
-}]);
-app.controller('PlokiPlokiCtl', function($scope, $rootScope, $location) {
-    var all = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
-    var spe = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split('');
+	function setValue(select, value) {
+		var current = select.value;
+		select.value = value;
+		if ( int(select.value) !== value ) {
+			select.value = current;
+			return false;
+		}
+		return true;
+	}
 
-    $scope.passlengths = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-    $scope.speciallengths = [0, 1, 2, 3, 4, 5];
+	function Ploki(passlength, spelength) {
+		var lengths = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+		var spelengths = [0, 1, 2, 3, 4, 5];
 
-    $scope.passlength = 12;
-    $scope.speciallength = 0;
+		this.all = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+		this.spe = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split('');
 
-    function genpassword() {
-        var i, idx, pass=[];
+		this.$passlength = document.querySelector('#password-length');
+		this.$spelength = document.querySelector('#special-length');
+		this.$input = document.querySelector('#plokipass');
 
-        for (i=0; i < $scope.passlength; i++) {
-            idx = (Math.random() * all.length) >>> 0;
-            pass.push(all[idx]);
-        }
-        for (i=$scope.speciallength; i > 0; i--) {
-            idx = (Math.random() * spe.length) >>> 0;
-            pass.push(spe[idx]);
-            pass.shift();
-        }
-        $scope.password = shuffle(pass).join('');
-    };
-    $scope.genpassword = genpassword;
+		initSelect(this.$passlength, lengths);
+		initSelect(this.$spelength, spelengths);
 
-    function hashme(hash) {
-        // Retrieve "config" information from hash
-        if (!hash) {
-            return false;
-        }
+		this.onChange = onChange.bind(this);
 
-        var num, match, changed=false;
+		this.$passlength.addEventListener('change', this.onChange);
+		this.$spelength.addEventListener('change', this.onChange);
+		this.$passlength.addEventListener('wheel', wheel.bind(this.$passlength, this));
+		this.$spelength.addEventListener('wheel', wheel.bind(this.$spelength, this));
 
-        match = /l(\d+)/.exec(hash);
-        num = match&& parseInt(match[1], 10);
-        if ( num && $scope.passlengths.indexOf(num) != -1 ) {
-            $scope.passlength = num;
-        }
+		document.querySelector('form').addEventListener('submit', this.onChange);
 
-        match = /s(\d+)/.exec(hash);
-        num = match && parseInt(match[1], 10);
-        if ( num && $scope.speciallengths.indexOf(num) != -1 ) {
-            $scope.speciallength = num;
-        }
-    }
-    $rootScope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
-        // Thanks
-        // http://stackoverflow.com/questions/3213531/creating-a-new-location-object-in-javascript
-        var a = document.createElement('a');
-        a.href = newUrl;
-        hashme(a.hash);
-    });
-    genpassword();
+		window.addEventListener('hashchange', hashChanged.bind(this));
 
-    $scope.$watch('passlength', genpassword);
-    $scope.$watch('speciallength', genpassword);
-});
+		this.$passlength.value = passlength;
+		this.$spelength.value = spelength;
+
+		if ( window.location.hash ) {
+			hashChanged.bind(this)();
+		} else {
+			this.onChange();
+		}
+		
+	}
+
+	function onChange(event) {
+		// this == ploki
+		if ( event && event.type == "submit" ) {
+			event.preventDefault();	
+		}
+
+		var i = 0,
+			s = this.$spelength.value,
+			l = this.$passlength.value - s,
+			idx,
+			pass = [];
+
+	    for (i=0; i < l; i++) {
+	        idx = (Math.random() * this.all.length) >>> 0;
+	        pass.push(this.all[idx]);
+	    }
+	    for (i=s; i > 0; i--) {
+	        idx = (Math.random() * this.spe.length) >>> 0;
+	        pass.push(this.spe[idx]);
+	    }
+
+	    this.$input.value = shuffle(pass).join('');
+	}
+
+	function wheel(ploki, e) {
+		e.preventDefault();
+
+		var value = this.value;
+
+		if ( e.deltaY > 0) {
+			value++;
+		} else if ( e.deltaY < 0) {
+			value--;
+		} else {
+			return;
+		}
+
+		if ( setValue(this, value) ) {
+			ploki.onChange();
+		}
+	}
+
+	function hashChanged() {
+		var hash = window.location.hash;
+		if (!hash) {
+	        return false;
+	    }
+
+	    var num, match, changed;
+
+	    match = /l(\d+)/.exec(hash);
+	    num = match && int(match[1])
+	    changed = num && setValue(this.$passlength, num);
+
+	    match = /s(\d+)/.exec(hash);
+	    num = match && int(match[1]);
+	    if ( num && setValue(this.$spelength, num) ) {
+	        changed = true;
+	    }
+
+	    if ( changed ) {
+	    	this.onChange();
+	    }
+	}
+
+	var ploki = new Ploki(12, 0);
+})();
